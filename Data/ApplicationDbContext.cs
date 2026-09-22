251879b5143e75fe0ccfb4924ea3910c96681403
JazzComia <jazzrussel.comia@antipolo.benilde.edu.ph>
2026-09-22 05:30:34 +0000

    additional

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ENTRPOG_ASP.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
